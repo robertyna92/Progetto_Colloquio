f9992427691a3676825cf3d6ea2d7751367017fc
Roberta Corallo <roxycrl92@gmail.com>
2021-12-15 11:30:08 +0100

    Basic Authentication
    A simple Authentication

---
+++ app_progetto/AuthenticatedUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace app_progetto
+{
+    public class AuthenticatedUser : IIdentity
+    {
+        public AuthenticatedUser(string autenticationType, bool isAuthenticated, string name)
+        {
+            AuthenticationType = autenticationType;
+            IsAuthenticated = isAuthenticated;
+            Name = name;
+        }
+
+        public string AuthenticationType { get; set; }
+
+        public bool IsAuthenticated { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ app_progetto/BasicAuthenticationHandler.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace app_progetto
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        IConfiguration _configuration;
+        public BasicAuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock, IConfiguration configuration) : base(options, logger, encoder, clock)
+        {
+
+            _configuration = configuration;
+        }
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            Response.Headers.Add("WWW-Authenticate", "Basic");
+
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Authorization header missing."));
+            }
+
+            // Get authorization key
+            var authorizationHeader = Request.Headers["Authorization"].ToString();
+            var authHeaderRegex = new Regex(@"Basic (.*)");
+
+            if (!authHeaderRegex.IsMatch(authorizationHeader))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Authorization code not formatted properly."));
+            }
+
+            var authBase64 = Encoding.UTF8.GetString(Convert.FromBase64String(authHeaderRegex.Replace(authorizationHeader, "$1")));
+            var authSplit = authBase64.Split(Convert.ToChar(":"), 2);
+            var authUsername = authSplit[0];
+            var authPassword = authSplit.Length > 1 ? authSplit[1] : throw new Exception("Unable to get password");
+
+            if (authUsername != "Roberta" || authPassword != "1234")
+            {
+                return Task.FromResult(AuthenticateResult.Fail("The username  is not correct."));
+            }
+
+            var authenticatedUser = new AuthenticatedUser("BasicAuth", true, "Roberta");
+            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(authenticatedUser));
+
+            return Task.FromResult(AuthenticateResult.Success(new AuthenticationTicket(claimsPrincipal, Scheme.Name)));
+
+        }
+    }
+}
